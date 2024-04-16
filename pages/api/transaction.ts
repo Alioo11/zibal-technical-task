902aad752c7a3c1d385b95e64590227353a43f86
Alioo11 <110mors110@gmail.com>
2024-04-16 10:27:24 +0330

    feat: implemented dummy API for transactions

---
+++ pages/api/transaction.ts
@@ -0,0 +1,26 @@
+import data from "@/data/transactions";
+import type { Transaction } from "@/types/transaction";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * @param {NextApiRequest} req
+ * @param {NextApiResponse<Array<Transaction>> }res
+ *
+ * @description dummy api to return mock transaction data :)
+ */
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Array<Transaction>>
+) {
+  const { trackId, cardNumber } = req.query;
+
+  const filteredTransactionData = data.filter((dataEntry) => {
+    const trackIdMatch =
+      !trackId || dataEntry.trackId.toString().includes(trackId.toString());
+    const cardNumberMatch =
+      !cardNumber || dataEntry.cardNumber.includes(cardNumber.toString());
+    return trackIdMatch && cardNumberMatch;
+  });
+
+  res.status(200).json(filteredTransactionData);
+}
---
+++ types/transaction.ts
@@ -0,0 +1,9 @@
+export interface Transaction {
+  amount: number;
+  trackId: number;
+  status: number;
+  paidAt: string;
+  cardNumber: string;
+}
+
+
