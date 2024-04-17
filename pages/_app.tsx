a17023b286370a5d9d56f264047413ce9186d634
Alioo11 <110mors110@gmail.com>
2024-04-17 11:59:15 +0330

    fix: fix nextjs headers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import AntDProvider from "@/providers/Antd";
 import type { AppProps } from "next/app";
+import 'antd/dist/reset.css';
+import "@/styles/globals.css";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AntDProvider>
+      <Component {...pageProps} />
+    </AntDProvider>
+  );
 }
+
